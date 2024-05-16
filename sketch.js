ff72d4c36a2c5881af44908d19cdfb6124c61aab
ifbastos <151560629+ifbastos@users.noreply.github.com>
2024-05-16 15:41:53 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(400, 400);
+    
+  }
+  
+  function draw() {
+    background(220);
+    strokeWeight(0.5);
+    //line(200, 0, 200, 400);
+    fill("black");
+    let palavra = "Ismael";
+    let palavr = "Freire";
+    let palav = "Bastos";
+    textSize(30);
+    text(palavra,200,180);
+    text(palav,203,260);
+    text(palavr,200,220);
+    textAlign(CENTER,CENTER);
+  
+  }
+  
